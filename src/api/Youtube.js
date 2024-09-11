f9162ac73fd79979dc22e22713041594792c36d2
ccrisolo <ccrisolo3@gmail.com>
2024-09-11 16:57:25 -0700

    update baseURL with api key

--- src/api/Youtube.js
+++ src/api/Youtube.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
 
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+const BASE_URL = "https://www.googleapis.com/youtube/v3/";
 //create will make GET requests
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
+    baseURL: BASE_URL,
 });
+
+export const getSearchedVideos = searchQuery => {
+    axios.get(BASE_URL`/search`, {
+        params: {
+            part: "snippet",
+            q: searchQuery,
+            key: API_KEY,
+        },
+    });
+};
