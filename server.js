81e1236389bfd4c4b724f79576070b3cc0160263
ccrisolo <ccrisolo3@gmail.com>
2024-09-12 14:46:48 -0700

    remove multiple app.listen in server.js

--- server.js
+++ server.js
@@ -3,48 +3,46 @@ const mongoose = require('mongoose');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cors = require('cors')
-require("dotenv").config();
+const cors = require('cors');
+require('dotenv').config();
 
-//connect to database
-require("./config/database");
+// Connect to database
+require('./config/database');
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('connected to database'))
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error('Database connection error:', error));
+db.once('open', () => console.log('Connected to database'));
 
-const favoritesRouter = require('./routes/api/favorites')
-
-app.listen(3000, () => console.log('server started'))
-
-app.use(cors({ // Use cors middleware
+// Use CORS middleware
+app.use(cors({
   origin: '*', // Adjust this to your frontend URL or a list of allowed URLs
 }));
-
 
 app.use(logger('dev'));
 app.use(express.json());
 
+// Serve static files from the React app
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Define API routes
+const usersRouter = require('./routes/api/users');
+const favoritesRouter = require('./routes/api/favorites');
 
-// Put API routes here, before the "catch all" route
-app.use('/api/users', require('./routes/api/users'));
+app.use('/api/users', usersRouter);
 app.use('/api/favorites', favoritesRouter);
 
-//'catch all' route below
-app.get('/*', function(req, res) {
+// 'Catch all' route for React app
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+});
 
-  // Configure to use port 3001 instead of 3000 during
-// development to avoid collision with React's dev server
+// Configure to use port from environment variable or default to 3001
 const port = process.env.PORT || 3001;
-
-app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
+app.listen(port, () => {
+  console.log(`Express app running on port ${port}`);
 });
